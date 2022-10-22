bea63fda0c97dee488aaa98e0a6d27a6d4ccf292
avedevios <106875633+avedevios@users.noreply.github.com>
2022-10-22 13:10:02 +0600

    Initial Commit

---
+++ netology-ui-2-navigation-prog/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  netology-ui-2-navigation-prog
+//
+//  Created by ake11a on 2022-10-22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
